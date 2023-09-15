90896fe4f37578602c4d0fb8279f4e1f93e5c905
heartbreathing <li.wang@hyperisland.se>
2023-09-15 22:04:48 +0200

    add hover and focus style

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -15,11 +15,11 @@ export const Navbar = () => {
   };
   return (
     <div className="navbar">
-      <Link to="/" >Home</Link>
-      <Link to="/create-recipe" >Create Recipe</Link>
-      <Link to="/saved-recipes" >Saved Recipes</Link>
+      <Link to="/" className="navbar-link">Home</Link>
+      <Link to="/create-recipe" className="navbar-link">Create Recipe</Link>
+      <Link to="/saved-recipes" className="navbar-link" >Saved Recipes</Link>
       {!cookies.access_token ? (
-        <Link to="/auth">Login/Register</Link>
+        <Link to="/auth" className="navbar-link">Login/Register</Link>
       ) : (
           <button className="log-button" onClick={logout} > Logout </button>
       )}
