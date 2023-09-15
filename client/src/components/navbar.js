abc6a896dc59f3cba8ee54cdf88f02dbb9423cc3
heartbreathing <li.wang@hyperisland.se>
2023-09-15 15:11:29 +0200

    modified some style

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -19,9 +19,9 @@ export const Navbar = () => {
       <Link to="/create-recipe" >Create Recipe</Link>
       <Link to="/saved-recipes" >Saved Recipes</Link>
       {!cookies.access_token ? (
-        <Link to="/auth" >Login/Register</Link>
+        <Link to="/auth">Login/Register</Link>
       ) : (
-          <button onClick={logout} > Logout </button>
+          <button className="log-button" onClick={logout} > Logout </button>
       )}
     </div>
   );
